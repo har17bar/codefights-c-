83403fd9ef9b7361abc1389ff233603ab0ab4a1b
Harut <har17bar@gmail.com>
2018-06-22 01:40:23 +0300

    Update and rename map.cpp to firstDuplicate map.cpp

---
+++ Interview Practice Arrays/FirstDuplicate/firstDuplicate map.cpp
@@ -0,0 +1,27 @@
+﻿
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+int firstDuplicate(std::vector<int> a) {
+	std::map<int, int> umap;
+	for (double item : a) {
+		if (umap[item]) {
+			return item;
+		}
+		umap[item] = true;
+	}
+	return -1;
+}
+
+int main()
+{
+	std::vector<int> a = { 2, 1, 3 , 2 , 1 , 5, 3, 2 };
+	cout << firstDuplicate(a);
+}
+
+
+
