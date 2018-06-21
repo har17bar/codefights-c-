af3047def018838006260ae7b4d6fb296964dad7
Harut <har17bar@gmail.com>
2018-06-21 23:46:54 +0300

    Update firstNotRepeatingCharacter.cpp

--- Interview Practice Arrays/FirstDuplicate/firstNotRepeatingCharacter.cpp
+++ Interview Practice Arrays/FirstDuplicate/firstNotRepeatingCharacter.cpp
@@ -1,27 +1,45 @@
 
 #include <iostream>
 #include <map>
-using namespace std;
+
+
 typedef std::map<char, int> MAP;
+char firstNotRepeatingCharacter(std::string s);
+using namespace std;
+
+int main(){
+	cout<<endl<<firstNotRepeatingCharacter("aab") << endl;
+}
+
 char firstNotRepeatingCharacter(std::string s) {
 
 	MAP repeat;
-	for (int i = 0; i < s.size(); ++i) {
-		if (repeat.count(s[i]) == 0) {
-			repeat[s[i]] = i;
+	int len = s.size();
+	int min;
+	char m;
+	if (len != 1) {
+		for (int i = len - 1; i >= 0; --i) {
+			if (repeat.count(s[i]) == 0) {
+				repeat[s[i]] = i;
+			}
+			else {
+				repeat[s[i]] = 0;
+			}
 		}
-		else {
-			repeat[s[i]] = 100;
+		min = repeat.begin()->second;
+		map<char, int>::const_iterator it;
+		for (it = repeat.begin(); it != repeat.end(); ++it) {
+			cout << it->first << "=" << it->second << endl;
+			if (it->second != 0 && (min == 0 || min>it->second)) {
+				min = it->second;
+				m = it->first;
+			}
 		}
 	}
-
-	return 0;
-}
-
-
-int main()
-{
-	cout<<firstNotRepeatingCharacter("abcacvfabhyad")<<endl;
+	else {
+		m = s[0];
+	}
+	return len == 1 || min != 0 ? m : '_';
 }
 
 
